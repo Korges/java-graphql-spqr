6a3c14492c19b0fa5bf8a6dfa2fa7045a54a3627
Korges <korgesik@gmail.com>
2020-04-06 22:40:37 +0200

    Update LectureService and LectureRepository.

--- src/main/java/com/korges/javagraphqlspqr/repository/LectureRepository.java
+++ src/main/java/com/korges/javagraphqlspqr/repository/LectureRepository.java
@@ -7,16 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
 
     @Query("SELECT lecture.subject FROM Lecture lecture WHERE lecture.id = :id")
-    Optional<Subject> findSubjectByGivenLecture(@Param("id") Long id);
+    Subject findSubjectByGivenLecture(@Param("id") Long id);
 
-
-
-
+    List<Lecture> findAllBySubject(@Param("subject") Subject subject);
 
 }
--- src/main/java/com/korges/javagraphqlspqr/service/LectureService.java
+++ src/main/java/com/korges/javagraphqlspqr/service/LectureService.java
@@ -1,50 +1,53 @@
 package com.korges.javagraphqlspqr.service;
 
+import com.korges.javagraphqlspqr.dto.input.LectureInput;
 import com.korges.javagraphqlspqr.entity.Lecture;
 import com.korges.javagraphqlspqr.entity.Subject;
 import com.korges.javagraphqlspqr.repository.LectureRepository;
 import com.korges.javagraphqlspqr.repository.SubjectRepository;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLContext;
-import io.leangen.graphql.annotations.GraphQLEnvironment;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
-import io.leangen.graphql.execution.ResolutionEnvironment;
 import io.leangen.graphql.execution.relay.Page;
 import io.leangen.graphql.execution.relay.generic.PageFactory;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @GraphQLApi
 @Service
 @RequiredArgsConstructor
 public class LectureService {
 
-    private final LectureRepository repository;
+    private final ModelMapper modelMapper;
+    private final LectureRepository lectureRepository;
     private final SubjectRepository subjectRepository;
 
-    @GraphQLQuery(name = "lectures", description = "aaaaaa")
-    public List<Lecture> findAllLectures() {
-        return repository.findAll();
+    @GraphQLQuery(name = "findAllLecturesByGivenSubject", description = "Retrieves list of all Lectures by given Subject")
+    public List<Lecture> findAllLecturesByGivenSubject(@GraphQLNonNull Long id) {
+        return subjectRepository.findById(id).map(lectureRepository::findAllBySubject)
+                .orElseThrow(() -> new EntityNotFoundException("Unable to find Lectures by given Subject id: " + id));
     }
 
-    @GraphQLQuery(name = "lecture")
-    public Optional<Lecture> findLectureById(@GraphQLEnvironment ResolutionEnvironment environment, Long id) {
-        return repository.findById(id);
+    @GraphQLQuery(name = "findLecture", description = "Retrieve single lecture by given id")
+    public Lecture findLectureById(@GraphQLNonNull Long id) {
+        return lectureRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Unable to find Subject with id: " + id));
     }
 
     @GraphQLQuery
     public Page<Lecture> lecturesPaged(@GraphQLArgument(name = "offset", defaultValue = "0") int offset,
                                        @GraphQLNonNull int size) {
-        org.springframework.data.domain.Page<Lecture> results = repository
+        org.springframework.data.domain.Page<Lecture> results = lectureRepository
                 .findAll(PageRequest.of(Math.max(offset, 0), Math.max(size, 1), Sort.by("id")));
 
         return PageFactory.createOffsetBasedPage(
@@ -53,13 +56,21 @@ public class LectureService {
                 offset * size);
     }
 
-    @GraphQLQuery(name = "subject", description = "dddddd")
-    public Optional<Subject> getSubjectByGivenLecture(@GraphQLContext Lecture lecture) {
-        return repository.findSubjectByGivenLecture(lecture.getId());
+    @GraphQLQuery(name = "subject", description = "Retrieve Subject by given Lecture")
+    public Subject findSubjectByGivenLecture(@GraphQLContext Lecture lecture) {
+        return lectureRepository.findSubjectByGivenLecture(lecture.getId());
     }
 
     @GraphQLMutation(name = "createLecture", description = "Creates a lecture entry")
-    public Lecture createLecture(@GraphQLArgument(name = "lecture", description = "The lecture object") @GraphQLNonNull @Valid Lecture lecture) {
-        return repository.save(lecture);
+    public Lecture createLecture(@GraphQLArgument(name = "lecture", description = "The lecture object") @GraphQLNonNull @Valid LectureInput lectureInput) {
+        return subjectRepository.findById(lectureInput.getSubject())
+                .map(subject -> saveLecture(subject, lectureInput))
+                .orElseThrow(() -> new EntityNotFoundException("Unable to find Lecture with id: " + lectureInput.getSubject()));
+    }
+
+    private Lecture saveLecture(Subject subject, LectureInput lectureInput) {
+        Lecture lecture = modelMapper.map(lectureInput, Lecture.class);
+        lecture.setSubject(subject);
+        return lectureRepository.save(lecture);
     }
 }
